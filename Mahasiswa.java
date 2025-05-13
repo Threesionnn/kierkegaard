4ef2106d3a6ef36972c53246b537adb306918dbc
Threesionnn <sionyehezkiel16@gmail.com>
2025-05-13 20:42:39 +0700

    Create Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,13 @@
+public class Mahasiswa {
+    String nim;
+    String nama;
+    String kelas;
+    String asal;
+
+    public Mahasiswa(String nim, String nama, String kelas, String asal) {
+        this.nim = nim;
+        this.nama = nama;
+        this.kelas = kelas;
+        this.asal = asal;
+    }
+}
